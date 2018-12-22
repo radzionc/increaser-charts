f20cc208aaccd06127f78d38995513039496404c
rodion <rodionaaafgh@gmail.com>
2018-12-22 11:57:48 +0300

    feature: size-aware container

--- example/src/App.js
+++ example/src/App.js
@@ -7,12 +7,45 @@ const Page = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  height: 60%;
+  width: 80%;
+  padding: 20px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const TimeWaitsForNoOne = styled.a`
+  margin: 40px;
+  color: white;
+  font-family: 'Dancing Script', cursive;
+  font-size: 24px;
+  cursor: pointer;
+  text-decoration: none;
 `
 
 export default () => {
   return (
     <Page>
-      <BarChart />
+      <Wrapper>
+        <BarChart />
+      </Wrapper>
+      <TimeWaitsForNoOne
+        target="_blank"
+        href="https://medium.com/@geekrodion/increaser-mindset-dc828a2bcd4d"
+      >
+        Time Waits For No One, and It Won't Wait For Me
+      </TimeWaitsForNoOne>
     </Page>
   )
 }
--- src/components/bar-chart/index.js
+++ src/components/bar-chart/index.js
@@ -3,14 +3,74 @@ import styled, { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from '../../constants/theme'
 
-const BarChart = styled.h1`
+const RootContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const BarsContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #FFD399;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const LabelsContainer = styled.div`
+  height: 40px;
+  background-color: #EF6C00;
+`
+
+const ScrollContainer = styled.div`
+  height: 20px;
+  background-color: #FC427B;
+`
+
+const BarsText = styled.h1`
   color: ${props => props.theme.textColor};
 `
 
-export default ({ theme }) => {
-  return (
-    <ThemeProvider theme={{ ...defaultTheme, ...theme}}>
-      <BarChart>Bar charts will be there</BarChart>
-    </ThemeProvider>
-  )
+export default class BarChart extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+  }
+
+  render() {
+    const { theme } = this.props
+
+    const { width, height } = this.state
+    const Bars = () => {
+      return <BarsText>{Math.round(width)} x {Math.round(height)}</BarsText>
+    }
+
+    return (
+      <ThemeProvider theme={{ ...defaultTheme, ...theme}}>
+        <RootContainer>
+          <BarsContainer ref={el => this.barsContainer = el}>
+            {width && height && <Bars/>}
+          </BarsContainer>
+          <LabelsContainer/>
+          <ScrollContainer/>
+        </RootContainer>
+      </ThemeProvider>
+    )
+  }
+
+  componentDidMount() {
+    this.onResize()
+    window.addEventListener('resize', this.onResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize)
+  }
+
+  onResize = () => {
+    const { width, height } = this.barsContainer.getBoundingClientRect()
+    this.setState({ width, height })
+  }
 }
